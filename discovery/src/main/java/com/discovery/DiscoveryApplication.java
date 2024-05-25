0203383c3c5ad2d55a2b5d14cd965df39fd58030
gimgitae <rlxo6919@chicment.com>
2024-05-25 09:44:45 +0900

    add

---
+++ discovery/src/main/java/com/discovery/DiscoveryApplication.java
@@ -0,0 +1,16 @@
+package com.discovery;
+
+import com.discovery.config.DiscoveryConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@Import({DiscoveryConfig.class})
+public class DiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryApplication.class, args);
+    }
+
+}
