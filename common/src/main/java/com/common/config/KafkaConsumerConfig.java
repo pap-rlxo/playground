10f9a069606f7af6b7fe1c20f626a7c70539e68c
gimgitae <rlxo6919@chicment.com>
2024-05-25 09:43:55 +0900

    kafka 추가

--- common/src/main/java/com/common/config/KafkaConsumerConfig.java
+++ common/src/main/java/com/common/config/KafkaConsumerConfig.java
@@ -22,7 +22,6 @@ public class KafkaConsumerConfig {
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_1");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-
         return new DefaultKafkaConsumerFactory<>(config);
     }
 
